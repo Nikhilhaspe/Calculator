20480b86396ff9d151a5e3e29469dd8e2ca299df
Nikhilhaspe <nikhilhaspe5220@gmail.com>
2022-05-06 15:24:21 +0530

    taking input through keyboard

--- script.js
+++ script.js
@@ -20,3 +20,20 @@ buttons.forEach((item) => {
     }
 });
 
+// Taking input through keyboard
+document.addEventListener('keydown', (event) => {
+    if (event.key == 'c' || event.key == 'C') {
+        display.innerText = '';
+    } else if (event.key == 'Backspace') {
+        let string = display.innerText.toString();
+        display.innerText = string.substr(0, string.length - 1);
+    } else if ((event.key == '=' || event.key == 'Enter') && display.innerText != '') {
+        display.innerText = eval(display.innerText);
+    } else if (display.innerText == '' && (event.key == '=' || event.key == 'Enter')) {
+        display.innerText = 'Empty!';
+        setTimeout(() => (display.innerText = ''), 2000);
+    } else if (numbers.includes(event.key)) {
+        display.innerText += event.key.toString();
+    }
+});
+
