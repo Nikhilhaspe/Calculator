931d03e91c26603ece83d5d991035594be2cc958
Nikhilhaspe <nikhilhaspe5220@gmail.com>
2022-05-06 15:23:35 +0530

    fetching all input buttons and setting on click listener

---
+++ script.js
@@ -0,0 +1,22 @@
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.', '+', '-', '*', '/'];
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((item) => {
+    item.onclick = () => {
+        if (item.id == 'clear') {
+            display.innerText = '';
+        } else if (item.id == 'backspace') {
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0, string.length - 1);
+        } else if (item.id == 'equal' && display.innerText != '') {
+            display.innerText = eval(display.innerText);
+        } else if (display.innerText == '' && item.id == 'equal') {
+            display.innerText = 'Empty!';
+            setTimeout(() => (display.innerText = ''), 2000);
+        } else if (item.id !== 'open' && item.id !== 'close') {
+            display.innerText += item.id;
+        }
+    }
+});
+
